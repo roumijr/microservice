23c2700c57a3c2f79671ed251d875b8a74d8512d
Mikhail Roghut`ko <pulsemrboy@gmail.com>
2020-12-28 15:21:47 +0200

    some fixes

--- cmd/working/handlers/hello.go
+++ cmd/working/handlers/hello.go
@@ -20,11 +20,14 @@ func NewHello(l *log.Logger) *Hello {
 
 
 func(h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	h.l.Println("hello world")
+	h.l.Println("Handle Hello Request")
+
+
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(rw, "Oops", http.StatusBadRequest)
-			
+		h.l.Println("Error reading body", err)
+
+		http.Error(rw, "Unable to read request body", http.StatusBadRequest)
 		return
 	}
 
--- cmd/working/main.go
+++ cmd/working/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os/signal"
+	"context"
+	"time"
 	"log"
 	"net/http"
 	"os"
@@ -9,15 +12,53 @@ import (
 	"working/cmd/working/handlers"
 )
 
+// BindAddress represents config for server connection 
+var BindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
 func main() {
+
+	env.Parse()
+
+
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
 
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 
-	http.ListenAndServe(":9090", sm)
+	// create a new server
+	s := &http.Server{
+		Addr: *BindAddress, 				// config 
+		Handler: sm,						// set the default handler
+		ErrorLog: l,						// set the logger for the server
+		WriteTimeout: 10 * time.Second,		// max time to write response to the client
+		ReadTimeout: 5 * time.Second,		// max time to read request from the client
+		IdleTimeout: 120 * time.Second,		// max time for connection using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		l.Printf("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s/n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interrupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	l.Println("Receive termenate , graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 } 
